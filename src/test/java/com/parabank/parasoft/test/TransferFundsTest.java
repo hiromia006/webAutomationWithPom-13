7ca6568430e9f2457f367d70be1bad13bbe25636
hiromia006 <hiromia006@gmail.com>
2025-10-07 22:55:36 +0600

    Add new Pages & Test Cases

--- src/test/java/com/parabank/parasoft/test/TransferFundsTest.java
+++ src/test/java/com/parabank/parasoft/test/TransferFundsTest.java
@@ -1,4 +1,45 @@
 package com.parabank.parasoft.test;
 
-public class TransferFundsTest extends BaseTest{
+import com.parabank.parasoft.pages.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TransferFundsTest extends BaseTest {
+    @Test
+    public void transferFundsV1ShouldSucceed() {
+        LoginPage loginPg = pg.goTo(LoginPage.class);
+
+        OverViewPage overViewPg = loginPg
+                .fillUsername(getUsername())
+                .fillPassword(getPassword())
+                .clickLoginBtn();
+        Assert.assertTrue(overViewPg.isLogOutLinkDisplayed());
+
+        TransferFundsPage openNewAccountPg = overViewPg
+                .clickTransferFundsLink();
+
+        int amount = 1000;
+        TransferCompletePage transferCompletePg = openNewAccountPg
+                .fillAmount(amount)
+                .selectFromAccount(0)
+                .selectToAccount(0)
+                .clickTransferButton();
+
+        Assert.assertTrue(transferCompletePg.isTransferComplete(amount));
+    }
+
+    @Test
+    public void transferFundsV2ShouldSucceed() {
+        int amount = 1000;
+        TransferCompletePage transferCompletePg = pg.goTo(LoginPage.class)
+                .fillUsername(getUsername())
+                .fillPassword(getPassword())
+                .clickLoginBtn()
+                .clickTransferFundsLink()
+                .fillAmount(amount)
+                .selectFromAccount(0)
+                .selectToAccount(0)
+                .clickTransferButton();
+        Assert.assertTrue(transferCompletePg.isTransferComplete(amount));
+    }
 }
